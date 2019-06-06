4940e66f0a35d797f1503c4aa3d31b0a8b92128d
favstats <fabio.votta@gmail.com>
2019-06-06 20:34:10 +0200

    add install_openmindR

--- R/helpers.R
+++ R/helpers.R
@@ -1,6 +1,7 @@
 #' Tidy cohen.d output
 #'
 #' This function creates a tidy dataframe from effsize::cohend.d
+#' @param cohendat results from Cohen's D
 #' @export
 tidy_cohen <- function(cohendat) {
   cohens_stuff %>%
@@ -11,4 +12,17 @@ tidy_cohen <- function(cohendat) {
     set_names(.[1,]) %>%
     .[-1,] %>% janitor::clean_names()
 }
+
+
+#' Install newest openmindR version from GitHub
+#'
+#' This function installs the newest openmindR version from GitHub
+#' @param ... arguments to install_github
+#' @export
+install_openmindR <- function(...) {
+
+  devtools::install_github("openmindplatform/openmindR", ...)
+
+}
+
 
